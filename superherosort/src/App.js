561afd0b4f34c0ebf603ff2e8c9299af54e9327e
wjhunter5315 <wjhunter5315@gmail.com>
2019-12-31 13:23:44 -0800

    trying to render heroes on load

--- superherosort/src/App.js
+++ superherosort/src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Table from "./components/Table/table";
+// import Table from "./components/Table/table";
+import Card from "./components/Card/card";
+import Sort from "./components/Sort/sort";
+import Filter from "./components/Filter/filter";
+import heroes from "./heroes.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Table/>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    heroes
+  };
+
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const heroes = this.state.heroes.filter(heroes => heroes.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ heroes });
+  };
+
+  render() {
+    return (
+      <div>
+      {this.state.heroes.map(heroes => {
+        <Card
+          removeFriend={this.removeFriend}
+          id={heroes.id}
+          name={heroes.name}
+          alias={heroes.alias}
+          team={heroes.team}
+        />
+      })}
+        <Sort/>
+        <Filter/>
+      </div>
+    );
+  };
 }
 
 export default App;
--- superherosort/src/components/Card/card.js
+++ superherosort/src/components/Card/card.js
@@ -1,75 +1,25 @@
 import React from "react";
 import "./card.css";
 
-function Card () {
+function Card (props) {
     return (
-        <li className="list-group-item"></li>
-    )
+        <div className="card">
+            <div className="content">
+                <ul>
+                    <li>
+                        <strong>Name:</strong> {props.name}
+                    </li>
+                    <li>
+                        <strong>Alias:</strong> {props.alias}
+                    </li>
+                    <li>
+                        <strong>Team:</strong> {props.team}
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+    );
 }
 
 export default Card;
-
-// class Card extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             name: "",
-//             alias: "",
-//             team: ""
-//         };
-//     }
-
-// }
-
-const heroes = [
-    {
-        name: "Tony Stark",
-        alias: "Ironman",
-        team: "Avengers"
-    },
-    {
-        name: "Natasha Rominoff",
-        alias: "Black Widow",
-        team: "Avengers"
-    },
-    {
-        name: "Bruce Banner",
-        alias: "The Hulk",
-        team: "Avengers"
-    },
-    {
-        name: "Steve Rogers",
-        alias: "Captain America",
-        team: "Avengers"
-    },
-    {
-        name: "Jean Grey",
-        alias: "Phoenix",
-        team: "X-Men"
-    },
-    {
-        name: "Scott Summers",
-        alias: "Cyclops",
-        team: "X-Men"
-    },
-    {
-        name: "Clarice Ferguson",
-        alias: "Blink",
-        team: "X-Men"
-    },
-    {
-        name: "Piotr Rasputin",
-        alias: "Colossus",
-        team: "X-Men"
-    },
-    {
-        name: "Matt Murdock",
-        alias: "Daredevil",
-        team: "Defenders"
-    },
-    {
-        name: "Danny Rand",
-        alias: "Iron Fist",
-        team: "Defender"
-    }
-]
